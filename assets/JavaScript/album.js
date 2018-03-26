24b01792bc57822b9addc72d3952530fc162768d
Jordyn Heinle <jordyn.heinle@hotmail.com>
2018-03-26 12:32:36 -0500

    Changed Band Name
    Changed band name to reflect different song

--- assets/JavaScript/album.js
+++ assets/JavaScript/album.js
@@ -2,9 +2,9 @@
  * This global array holds our collection of albums
  */
 var albums = [{
-  title: "The White Album",
-  band:  "The Beatles",
-  year:  1968
+  title: "End Game",
+  band:  "Taylor Swift",
+  year:  2017
 }];
 
 
